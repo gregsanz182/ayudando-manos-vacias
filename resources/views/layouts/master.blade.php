d3d770a3051d217d8f47b72ad51b6a1f44b075ce
gregsanz182 <sanchz93@gmail.com>
2017-10-14 12:07:22 -0400

    Cambiados estilos a less

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ URL::to('bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ URL::to('font-awesome/css/font-awesome.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::to('css/style.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::to('bootstrap-select-1.12.4/dist/css/bootstrap-select.min.css') }}">
     <title>Ayudando Manos Vacias - Inicio</title>
