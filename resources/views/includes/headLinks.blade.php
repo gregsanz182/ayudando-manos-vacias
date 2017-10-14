3e9575b0eab72c41fc3b5b2459ef71abf1f36b6c
gregsanz182 <sanchz93@gmail.com>
2017-10-14 15:20:05 -0400

    Acomodado header. Arreglado links a bootstrap-select.

--- resources/views/includes/headLinks.blade.php
+++ resources/views/includes/headLinks.blade.php
@@ -1,5 +1,4 @@
 <link rel="stylesheet" href="{{ URL::to('bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::to('bootstrap-select-1.12.4/css/bootstrap-select.min.css') }}">
 <link rel="stylesheet" href="{{ URL::to('font-awesome/css/font-awesome.min.css') }}">
 <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
-<link rel="stylesheet" href="{{ URL::to('bootstrap-select-1.12.4/dist/css/bootstrap-select.min.css') }}">
