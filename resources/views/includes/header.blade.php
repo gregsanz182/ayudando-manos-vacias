3d50f702b0208a8a566763a0b08bbbbf14dd5dd0
gregsanz182 <sanchz93@gmail.com>
2017-10-14 17:34:41 -0400

    Arreglada la posicion del header. Incluido nueva seccion iniciar sesion.

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,20 +1,76 @@
 <nav class="navbar navbar-default navbar-fixed-top navbar-home">
     <div class="container">
-        <div class="row row-aligned">
-            <div class="col-md-2">
-                <a href="#" class="navbar-brand"><img src="img/logo.png" alt="Inicio"></a>
+        <div class="row">
+            <div class="col">
+                <a href="#">
+                    <img src="img/logo.png" alt="Inicio" class="logo-img">
+                </a>
             </div>
-            <div class="col-md-9 row-align-bottom">
-                <ul class="nav navbar-nav">
-                    <li><a href="index.html" class="active">Inicio</a></li>
-                    <li><a href="buscar_nino.html">Buscar</a></li>
-                    <li><a href="ayuda.html">Ayuda</a></li>
-                </ul>
-            </div>
-            <div class="col-md-1 row-align-bottom">
-                <button class="btn btn-deafult navbar-btn button-reg" submit="registrarse.html">
-                    Registrarse
-                </button>
+            <div class="col-xs-12 header-right">
+                <div class="row header-row">
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="index.html" class="active">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="buscar_nino.html">Buscar</a>
+                        </li>
+                        <li>
+                            <a href="ayuda.html">Ayuda</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <b>Login</b>
+                                <span class="caret"></span>
+                            </a>
+                            <ul id="login-dp" class="dropdown-menu">
+                                <li>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            Login via
+                                            <div class="social-buttons">
+                                                <a href="#" class="btn btn-fb">
+                                                    <i class="fa fa-facebook"></i> Facebook</a>
+                                                <a href="#" class="btn btn-tw">
+                                                    <i class="fa fa-twitter"></i> Twitter</a>
+                                            </div>
+                                            or
+                                            <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                                                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label class="sr-only" for="exampleInputPassword2">Password</label>
+                                                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                                                    <div class="help-block text-right">
+                                                        <a href="">Forget the password ?</a>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                                                </div>
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox"> keep me logged-in
+                                                    </label>
+                                                </div>
+                                            </form>
+                                        </div>
+                                        <div class="bottom text-center">
+                                            New here ?
+                                            <a href="#">
+                                                <b>Join Us</b>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
