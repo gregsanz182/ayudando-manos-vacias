ea5b108646b2304a115e72d7ae2c7bfcced402f4
gregsanz182 <sanchz93@gmail.com>
2017-10-14 19:26:11 -0400

    Listo el formulario de ingreso

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -22,49 +22,31 @@
                     <ul class="nav navbar-nav navbar-right">
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <b>Login</b>
+                                <b>Inicia Sesión</b>
                                 <span class="caret"></span>
                             </a>
                             <ul id="login-dp" class="dropdown-menu">
                                 <li>
                                     <div class="row">
-                                        <div class="col-md-12">
-                                            Login via
-                                            <div class="social-buttons">
-                                                <a href="#" class="btn btn-fb">
-                                                    <i class="fa fa-facebook"></i> Facebook</a>
-                                                <a href="#" class="btn btn-tw">
-                                                    <i class="fa fa-twitter"></i> Twitter</a>
+                                        <form action="" class="login-form">
+                                            <h4>Ingresa con tu cuenta</h4>
+                                            <div class="form-group">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                                    <input type="text" class="form-control" placeholder="Usuario" name="log-user-input">
+                                                </div>
                                             </div>
-                                            or
-                                            <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                                <div class="form-group">
-                                                    <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                                    <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                                            <div class="form-group">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                                                    <input type="text" class="form-control" placeholder="Usuario" name="log-pass-input">
                                                 </div>
-                                                <div class="form-group">
-                                                    <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                                    <div class="help-block text-right">
-                                                        <a href="">Forget the password ?</a>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                                </div>
-                                                <div class="checkbox">
-                                                    <label>
-                                                        <input type="checkbox"> keep me logged-in
-                                                    </label>
-                                                </div>
-                                            </form>
-                                        </div>
-                                        <div class="bottom text-center">
-                                            New here ?
-                                            <a href="#">
-                                                <b>Join Us</b>
-                                            </a>
-                                        </div>
+                                            </div>
+                                            <button type="button" class="btn btn-default boton-login">Ingresar</button>
+                                        </form>
+                                    </div>
+                                    <div class="row foot">
+                                        <span>¿No tienes cuenta? <a href="#">Registrate</a></span>
                                     </div>
                                 </li>
                             </ul>
