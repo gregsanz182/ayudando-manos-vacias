3b8d944c3814c482341f0498003a8832f3fbf5d1
gregsanz182 <sanchz93@gmail.com>
2017-10-15 14:16:52 -0400

    Creado migrate para botacora y admin

---
+++ database/migrations/2017_10_15_181319_create_bitacora_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBitacoraTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacora', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('accion');
+            $table->string('tabla');
+            $table->integer('usuario_id')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('bitacora', function (Blueprint $table) {
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacora');
+    }
+}
