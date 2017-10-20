67ac80701c6c39fcd9940f5b0016cdf229f8a7c9
gregsanz182 <sanchz93@gmail.com>
2017-10-20 19:19:35 -0400

    Vista principal gestion requerimientos terminada

--- app/Http/Controllers/RequerimientoController.php
+++ app/Http/Controllers/RequerimientoController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Nino_Medicamento;
+use App\Nino_Insumo;
 
 class RequerimientoController extends Controller
 {
     public function gestionRequerimientos($nino_id)
     {
-        $meds = Nino_Medicamento::where('nino_id', $nino_id)->orderBy('created_at')->get();
-        return view('ges_requerimientos', ['meds' => $meds]);
+        $medicamentos = Nino_Medicamento::where('nino_id', $nino_id)->orderBy('created_at', 'DESC')->paginate(10);
+        $insumos = Nino_Insumo::where('nino_id', $nino_id)->orderBy('created_at', 'DESC')->paginate(10);
+        return view('ges_requerimientos', ['medicamentos' => $medicamentos, 'insumos' => $insumos]);
     }
 }
--- resources/views/ges_requerimientos.blade.php
+++ resources/views/ges_requerimientos.blade.php
@@ -9,30 +9,81 @@
         </div>
     </div>
     <div class="container" id="cuerpo">
-        <form class="col-xs-12" method='post' action="">
+        <div class="row tabla-reqs">
             <h3>Medicamentos</h3>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Otro nombre</th>
-                        <th>Dosis</th>
-                        <th>Estado</th>
-                        <th>Operaciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($meds as $med)
-                        <tr>
-                            <th>{{ $med->medicamento->nombre }}</th>
-                            <th>{{ $med->nombre_otro }}</th>
-                            <th>{{ $med->dosis }}</th>
-                            <th>{{ $med->estado_requerimiento }}</th>
-                            <th></th>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </form>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 text-right paginacion-meds-ins">
+                    {{ $medicamentos->links() }}
+                    <button type="button" class="btn btn-success"><i class="fa fa-plus"></i> Añadir medicamento</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <table class="table table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Otro nombre</th>
+                                <th>Fecha limite</th>
+                                <th>Dosis</th>
+                                <th>Estado</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($medicamentos as $medicamento)
+                                <tr>
+                                    <th>{{ $medicamento->medicamento->nombre }}</th>
+                                    <th>{{ $medicamento->nombre_otro }}</th>
+                                    <th>{{ $medicamento->fecha }}</th>
+                                    <th>{{ $medicamento->dosis }}</th>
+                                    <th>{{ $medicamento->estado_requerimiento }}</th>
+                                    <th><button type="button" class="btn btn-link" data-med-id=''><i class="fa fa-edit"></i></button></th>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="row tabla-reqs">
+            <h3>Insumos</h3>
+            <hr>
+            <div class="row">
+                <div class="col-xs-12 text-right paginacion-meds-ins">
+                    {{ $insumos->links() }}
+                    <button type="button" class="btn btn-success"><i class="fa fa-plus"></i> Añadir insumo</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12">
+                    <table class="table table-hover table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Categoria</th>
+                                <th>Fecha limite</th>
+                                <th>Motivo</th>
+                                <th>Estado</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($insumos as $insumo)
+                                <tr>
+                                    <th>{{ $insumo->nombre }}</th>
+                                    <th>{{ $insumo->categoria_insumo->nombre }}</th>
+                                    <th>{{ $insumo->fecha }}</th>
+                                    <th>{{ $insumo->motivo }}</th>
+                                    <th>{{ $insumo->estado_requerimiento }}</th>
+                                    <th><button type="button" class="btn btn-link" data-ins-id=''><i class="fa fa-edit"></i></button></th>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
