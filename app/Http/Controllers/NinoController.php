09dc64d3bfa54ded0f27e5cacc4d5b29c6d040c9
gregsanz182 <sanchz93@gmail.com>
2017-10-20 01:15:43 -0400

    Registro nino completo

--- app/Http/Controllers/NinoController.php
+++ app/Http/Controllers/NinoController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Nino;
 use App\Localidad;
 use App\Cancer;
 use App\Medicamento;
 use App\Categoria_Insumo;
+use App\Nino_Cancer;
 
 class NinoController extends Controller
 {
@@ -69,15 +71,41 @@ class NinoController extends Controller
         $this->validate($request, [
             'nombre' => 'required',
             'apellido' => 'required',
-            'fecha_nacimiento' => 'required|date_format:d/m/Y',
+            'fecha_nacimiento' => 'required|date_format:Y-m-d',
             'genero' => 'required',
-            'relacion_representante' => 'required',
             'identificacion' => 'nullable',
             'situacion_actual' => 'required|max:250',
             'tipo_cancer' => 'required',
-            'estado_actual_cancer' => 'required|max:100',
-            'fecha_desde' => 'required|date_format:d/m/Y'
+            'estado_actual_cancer' => 'nullable|max:100',
+            'fecha_desde' => 'required|date_format:Y-m-d',
+            'relacion_representante' => 'required|numeric'
         ]);
+        
+        $nino = new Nino;
+        $nino->nombre = $request['nombre'];
+        $nino->apellido = $request['apellido'];
+        $nino->genero = $request['genero']==1?'M':'F';
+        $nino->fecha_nacimiento = $request['fecha_nacimiento'];
+        $nino->situacion_actual = $request['situacion_actual'];
+        $nino->relacion_repr = Nino::$relacionesRepr[(int)$request['relacion_representante']];
+        if ($request->has('identificacion'))
+        {
+            $nino->identificacion = $request['identificacion'];
+        }
+        $nino->representante_id = Auth::user()->id;
+
+        $nino->save();
+
+        $nino_cancer = new Nino_Cancer;
+        $nino_cancer->fecha_desde = $request['fecha_desde'];
+        if($request->has('estado_actual_cancer'))
+        {
+            $nino_cancer->estado_actual = $request['estado_actual_cancer'];
+        }
+        $nino_cancer->nino_id = $nino->id;
+        $nino_cancer->cancer_id = $request['tipo_cancer'];
+
+        $nino_cancer->save();
 
         return redirect()->route('inicio');
     }
